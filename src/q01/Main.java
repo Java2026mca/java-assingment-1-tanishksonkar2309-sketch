655cdd2a4226deffceb20599a34353a9847e5a1c
tanishksonkar2309-sketch <tanishksonkar2309@gmail.com>
2026-04-07 12:22:43 +0530

    Implement hollow diamond printing method
    Added a method to print a hollow diamond shape based on the input size.

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -20,6 +20,37 @@ public class Main {
         //  *   *
         //   * *
         //    *
+        public static void printHollowDiamond(int n) {
+    // Upper half and middle
+    for (int i = 0; i < n; i++) {
+        // Leading spaces
+        for (int j = 0; j < n - i - 1; j++) System.out.print(" ");
+        
+        System.out.print("*");
+        
+        if (i > 0) {
+            // Inner spaces: (2 * row_index) - 1
+            for (int j = 0; j < (2 * i) - 1; j++) System.out.print(" ");
+            System.out.print("*");
+        }
+        System.out.println();
+    }
 
+    // Lower half
+    for (int i = n - 2; i >= 0; i--) {
+        // Leading spaces
+        for (int j = 0; j < n - i - 1; j++) System.out.print(" ");
+        
+        System.out.print("*");
+        
+        if (i > 0) {
+            // Inner spaces
+            for (int j = 0; j < (2 * i) - 1; j++) System.out.print(" ");
+            System.out.print("*");
+        }
+        System.out.println();
     }
 }
+
+    
+
