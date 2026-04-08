7b21385f1bf0c9603667574b9293255bbadd862f
tanishksonkar2309-sketch <tanishksonkar2309@gmail.com>
2026-04-08 14:49:36 +0530

    Add spiral matrix filling and diagonal sum calculation
    Implemented a method to fill a matrix in a spiral order and calculate the diagonal sum.

--- src/q03/Main.java
+++ src/q03/Main.java
@@ -15,6 +15,46 @@ public class Main {
         // 8 9 4
         // 7 6 5
         // Diagonal: 15
+int[][] matrix = new int[n][n];
 
+        int value = 1;
+        int top = 0, bottom = n - 1;
+        int left = 0, right = n - 1;
+
+        // Fill matrix spirally
+        while (top <= bottom && left <= right) {
+            // Move Right
+            for (int i = left; i <= right; i++) matrix[top][i] = value++;
+            top++;
+
+            // Move Down
+            for (int i = top; i <= bottom; i++) matrix[i][right] = value++;
+            right--;
+
+            // Move Left
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) matrix[bottom][i] = value++;
+                bottom--;
+            }
+
+            // Move Up
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) matrix[i][left] = value++;
+                left++;
+            }
+        }
+
+        // Print Matrix and Calculate Diagonal
+        int diagonalSum = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + (j == n - 1 ? "" : " "));
+                if (i == j) {
+                    diagonalSum += matrix[i][j];
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("Diagonal: " + diagonalSum);
     }
 }
