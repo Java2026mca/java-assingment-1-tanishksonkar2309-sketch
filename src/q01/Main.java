b4decf78034a2592f4cc91881dea94af8fc829a7
tanishksonkar2309-sketch <tanishksonkar2309@gmail.com>
2026-04-09 13:35:54 +0530

    Implement hollow diamond pattern printing

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-    }
+    
         // TODO: Print a hollow diamond pattern of half-height n using '*'
         //
         // Rules:
@@ -20,39 +20,39 @@ public class Main {
         //  *   *
         //   * *
         //    *
-        public static void printHollowDiamond(int n) {
-    // Upper half and middle
-    for (int i = 0; i < n; i++) {
-        // Leading spaces
-        for (int j = 0; j < n - i - 1; j++) {
-            System.out.print(" ");
-        
-        System.out.print("*");
-        
-        if (i > 0) {
-            // Inner spaces: (2 * row_index) - 1
-            for (int j = 0; j < (2 * i) - 1; j++) System.out.print(" ");
-            System.out.print("*");
+       for (int i = 1; i <= n; i++) {
+            // Print leading spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // Print hollow stars
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(); // No trailing spaces
         }
-        System.out.println();
-    }
-    }
-    // Lower half
-    for (int i = n - 2; i >= 0; i--) {
-        // Leading spaces
-        for (int j = 0; j < n - i - 1; j++) {
-            System.out.print(" ");
-        
-        System.out.print("*");
-        
-        if (i > 0) {
-            // Inner spaces
-            for (int j = 0; j < (2 * i) - 1; j++) System.out.print(" ");
-            System.out.print("*");
-        }
-        System.out.println();
+
+        // Lower half (rows n-1 down to 1)
+        for (int i = n - 1; i >= 1; i--) {
+            // Print leading spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // Print hollow stars
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(); // No trailing spaces
+        } 
     }
 }
 
 
-    }
