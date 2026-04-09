65e3ba006483258b66aa93d49fd07c40915923b6
tanishksonkar2309-sketch <tanishksonkar2309@gmail.com>
2026-04-09 10:56:37 +0530

    Add stack-based evaluation for reverse Polish notation
    Implemented a stack-based evaluation of reverse Polish notation to compute the result of mathematical expressions.

--- src/q10/Main.java
+++ src/q10/Main.java
@@ -16,6 +16,29 @@ public class Main {
         // Explanation: 5 + ((1+2)*4) - 3 = 5 + 12 - 3 = 14
 
         String line = sc.nextLine();
+ String[] tokens = line.split("\\s+");
+        Stack<Integer> stack = new Stack<>();
 
+        for (String token : tokens) {
+            // Check if token is an operator
+            if (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")) {
+                // Pop the top two elements (order matters for - and /)
+                int val2 = stack.pop();
+                int val1 = stack.pop();
+
+                switch (token) {
+                    case "+": stack.push(val1 + val2); break;
+                    case "-": stack.push(val1 - val2); break;
+                    case "*": stack.push(val1 * val2); break;
+                    case "/": stack.push(val1 / val2); break; // Integer division
+                }
+            } else {
+                // If not an operator, it must be an operand
+                stack.push(Integer.parseInt(token));
+            }
+        }
+
+        // The final result is the only item left in the stack
+        System.out.println(stack.pop());
     }
 }
