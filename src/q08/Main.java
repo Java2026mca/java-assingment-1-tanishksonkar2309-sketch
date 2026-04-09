e9fcda6549e75683e574c6cf66476f696cf69f39
tanishksonkar2309-sketch <tanishksonkar2309@gmail.com>
2026-04-09 10:51:22 +0530

    Implement stack operations: PUSH, POP, PEEK, SIZE

--- src/q08/Main.java
+++ src/q08/Main.java
@@ -26,6 +26,34 @@ public class Main {
         // 20
         // 10
         // EMPTY
+int[] stack = new int[n];
+        int top = -1; // Index of the current top element
 
+        for (int i = 0; i < n; i++) {
+            String command = sc.next();
+
+            if (command.equalsIgnoreCase("PUSH")) {
+                int value = sc.nextInt();
+                stack[++top] = value; // Increment top and insert
+            } 
+            else if (command.equalsIgnoreCase("POP")) {
+                if (top == -1) {
+                    System.out.println("EMPTY");
+                } else {
+                    System.out.println(stack[top--]); // Print and decrement top
+                }
+            } 
+            else if (command.equalsIgnoreCase("PEEK")) {
+                if (top == -1) {
+                    System.out.println("EMPTY");
+                } else {
+                    System.out.println(stack[top]); // Print current top without removing
+                }
+            } 
+            else if (command.equalsIgnoreCase("SIZE")) {
+                System.out.println(top + 1); // Size is always top index + 1
+            }
+        }
+        sc.close();
     }
 }
