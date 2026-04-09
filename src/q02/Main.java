d7ea29f45b0322bba277a79e3b03324e8bf60978
tanishksonkar2309-sketch <tanishksonkar2309@gmail.com>
2026-04-09 13:59:37 +0530

    Add method to print Pascal's Triangle
    Implemented the printPascalsTriangle method to display Pascal's Triangle for a given number of rows.

--- src/q02/Main.java
+++ src/q02/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        printPascalsTriangle(n);
+    }
 
         // TODO: Print Pascal's Triangle for n rows
         //       Values on each row separated by single space
@@ -16,44 +18,25 @@ public class Main {
         // 1 2 1
         // 1 3 3 1
         // 1 4 6 4 1
-        for (int i = 0; i < n; i++) {
-            int number = 1;
-            for (int j = 0; j <= i; j++) {
-                System.out.print(number);
-                // Calculate next number using Binomial Coefficient property: 
-                // nCr = (nCr-1 * (n - r)) / r
-                number = number * (i - j) / (j + 1);
-                
-                // Add space only if it's not the last element in the row
-                if (j < i) {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-        }
-        sc.close();
-
-    }
-}
-   /* public static void printPascalsTriangle(int n) {
+        public static void printPascalsTriangle(int n) {
         for (int i = 0; i < n; i++) {
             int number = 1;
             for (int j = 0; j <= i; j++) {
                 // Print the current number
                 System.out.print(number);
                 
-                // Print a space only if it is not the last element in the row
+                // Print a space only if it's not the last number in the row
                 if (j < i) {
                     System.out.print(" ");
                 }
-                
-                // Calculate the next number in the row using the binomial coefficient property:
-                // C(n, k) = C(n, k-1) * (n - k + 1) / k
+
+                // Calculate the next number in the row using binomial coefficients:
+                // C(n, k+1) = C(n, k) * (n - k) / (k + 1)
                 number = number * (i - j) / (j + 1);
             }
             // Move to the next line after each row
             System.out.println();
         }
         
-    }*/
+    
 }
